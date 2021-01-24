0538d9b72b4a58659a0bca63c12ef1afd288d6e9
Yandy Ramirez <yandy@cloudart.io>
2021-01-24 13:18:22 -0500

    add beets and necessary configuration

---
+++ scripts/beet_import.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+BEET_CONFIG="/config/config.yaml"
+
+echo "$lidarr_addedtrackpaths"
+lidarr_first_track=$(echo "$lidarr_addedtrackpaths" | cut -d '|' -f1)
+lidarr_album_path=$(dirname "$lidarr_first_track")
+echo "Path: $lidarr_album_path"
+echo "Album MBID: $lidarr_album_mbid"
+echo "Release MBID: $lidarr_albumrelease_mbid"
+echo "Running beet import"
+
+docker --config "/config/.docker" exec beets beet -c $BEET_CONFIG update "path:$lidarr_album_path" || true
+docker --config "/config/.docker" exec beets beet -c $BEET_CONFIG import -q --search-id "$lidarr_albumrelease_mbid" "$lidarr_album_path"
